31d69d2d84e28bbe2674353e9226d03b5b548fae
ngvuong <vminguyen@ucdavis.edu>
2022-07-09 00:03:00 -0700

    Update Layout with metadata and Navbar

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,22 +1,24 @@
 import Head from 'next/head';
+import Navbar from './Navbar';
 
 import styles from '../../styles/Layout.module.scss';
 
-export default function Layout(children) {
+export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Productivity Level Up</title>
+        <title>Plum Tracker</title>
         <meta
           name='description'
           content='A simple experience tracker, level up your productivity'
         />
         <meta
           name='keywords'
-          content='web development, nextjs, productivity, level up, experience, tracker, time, pomodoro'
+          content='web development, nextjs, productivity, level up, experience, exp tracker, time, pomodoro, plum, productivity level up mate'
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <Navbar />
       <div className={styles.container}>{children}</div>
     </>
   );
