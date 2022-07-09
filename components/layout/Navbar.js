0b38578adb1cf150ef1f66d64e3d7c6124fb4420
ngvuong <vminguyen@ucdavis.edu>
2022-07-09 02:18:32 -0700

    Update Navbar links, styles, layout

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,5 +1,48 @@
+import Link from 'next/link';
+
+import {
+  RiHome4Fill,
+  RiTimerFill,
+  RiPlantFill,
+  RiBarChartFill,
+  RiSettings5Fill,
+  RiLogoutBoxFill,
+} from 'react-icons/ri';
 import styles from '../../styles/Navbar.module.scss';
 
 export default function Navbar() {
-  return <nav className={styles.navbar}></nav>;
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <Link href='/home'>
+          <a>
+            <RiHome4Fill />
+          </a>
+        </Link>
+        <Link href='/timer'>
+          <a>
+            <RiTimerFill />
+          </a>
+        </Link>
+        <Link href='/garden'>
+          <a>
+            <RiPlantFill />
+          </a>
+        </Link>
+        <Link href='/stats'>
+          <a>
+            <RiBarChartFill />
+          </a>
+        </Link>
+        <Link href='/settings'>
+          <a>
+            <RiSettings5Fill />
+          </a>
+        </Link>
+        <button>
+          <RiLogoutBoxFill />
+        </button>
+      </nav>
+    </>
+  );
 }
