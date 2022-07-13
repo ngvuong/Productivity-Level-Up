a272f8c08750fdf0a4806125a750b3cf4712a013
ngvuong <vminguyen@ucdavis.edu>
2022-07-12 23:52:25 -0700

    Update Level logic, styles

--- components/ui/Level.js
+++ components/ui/Level.js
@@ -1,25 +1,30 @@
-import { useState } from 'react';
 import styles from '../../styles/Level.module.scss';
 
-export default function Level() {
-  const [p, setP] = useState(90);
+export default function Level({ user }) {
+  const percent = +((user.exp / user.expNext) * 100).toFixed(2);
+
   return (
     <div className={styles.container}>
-      <div className={styles.level}>Level 1</div>
+      <div className={styles.wrapper}>
+        <div className={styles.level}>Level {user.level} </div>
+        <div className={styles.streak}>
+          Current Non-Zero Streak: {user.streak}
+        </div>
+      </div>
       <div className={styles.exp}>
         <div className={styles.expBar}>
           <div
             className={styles.expBarFill}
             style={{
-              width: `${p}%`,
-              borderRadius: `${p >= 100 ? '1rem' : '1rem 0 0 1rem'}`,
+              width: `${percent}%`,
+              borderRadius: `${percent >= 100 ? '1rem' : '1rem 0 0 1rem'}`,
             }}
           />
           <span></span>
           <span></span>
           <span></span>
         </div>
-        <span>50/100 ({p + '%'})</span>
+        <span>{`${user.exp}/${user.expNext} (${percent}%)`}</span>
       </div>
     </div>
   );
