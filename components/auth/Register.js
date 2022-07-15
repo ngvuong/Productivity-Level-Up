ef1c3cb85e5150ee856a37e6544900c71b3ced6b
ngvuong <vminguyen@ucdavis.edu>
2022-07-14 21:17:23 -0700

    Add basic Register component

---
+++ components/auth/Register.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Register.module.scss';
+
+export default function Register() {
+  return (
+    <div className={styles.register}>
+      <h2>Sign up</h2>
+    </div>
+  );
+}
