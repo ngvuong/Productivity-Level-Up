f0aa03f04369fc938881b6f3ca8dfcd0034911b5
ngvuong <vminguyen@ucdavis.edu>
2022-07-15 00:24:48 -0700

    Update Register social login buttons

--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,9 +1,34 @@
+import { FaGithub, FaGoogle, FaFacebook, FaSignInAlt } from 'react-icons/fa';
 import styles from '../../styles/Register.module.scss';
 
-export default function Register() {
+export default function Register({ setIsLogin }) {
   return (
     <div className={styles.register}>
-      <h2>Sign up</h2>
+      <h2>Sign Up</h2>
+      <hr />
+      <div className={styles.loginBtns}>
+        <button className={styles.github}>
+          <FaGithub />
+          GitHub
+        </button>
+        <button className={styles.google}>
+          <FaGoogle />
+          Google
+        </button>
+        <button className={styles.facebook}>
+          <FaFacebook />
+          Facebook
+        </button>
+      </div>
+      <span>
+        <hr />
+        Or
+        <hr />
+      </span>
+      <button className={styles.login} onClick={() => setIsLogin(true)}>
+        <FaSignInAlt />
+        Log in
+      </button>
     </div>
   );
 }
