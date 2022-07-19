20678ba5af8d52c99911648ef4673a249809c918
ngvuong <vminguyen@ucdavis.edu>
2022-07-18 23:58:38 -0700

    Update Clock

--- components/ui/Clock.js
+++ components/ui/Clock.js
@@ -1,27 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import styles from '../../styles/Clock.module.scss';
 
-export default function Clock({ duration, start = false }) {
+export default function Clock({ duration, breakDuration, run, onDone }) {
   const [time, setTime] = useState(duration * 60);
+  const count = useRef(null);
 
   useEffect(() => {
     setTime(duration * 60);
   }, [duration]);
 
   useEffect(() => {
-    if (start && time > 0) {
+    if (run && time > 0) {
       const interval = setInterval(() => {
-        setTime((prevTime) => prevTime - 1);
-      }, 1000);
+        setTime((prevTime) => --prevTime);
+      }, 100);
 
       return () => clearInterval(interval);
+    } else if (time === 0) {
+      onDone();
+      count.current = breakDuration > 0 ? ++count.current : 0;
+      console.log(count);
+      if (count.current % 2 === 0) {
+        setTime(duration * 60);
+      } else {
+        setTime(breakDuration * 60);
+      }
     }
-  }, [start, time]);
+  }, [run, time, breakDuration]);
 
   return (
     <div className={styles.clock}>
-      {`${Math.floor(time / 60)}:${(time % 60).toLocaleString('en-US', {
+      {`${Math.floor(time / 60).toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGroup: false,
+      })}:${(time % 60).toLocaleString('en-US', {
         minimumIntegerDigits: 2,
         useGroup: false,
       })}`}
