46304372b832cac316b3a7f4ac364e44c5c79e91
ngvuong <vminguyen@ucdavis.edu>
2022-07-22 00:11:04 -0700

    Update Layout with authentication, re-routing

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,9 +1,25 @@
+import { useEffect, cloneElement } from 'react';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
-import Navbar from './Navbar';
+import Navbar from '../ui/Navbar';
+import { useUser } from '../../contexts/userContext';
 
 import styles from '../../styles/Layout.module.scss';
 
 export default function Layout({ children }) {
+  const { user, status } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === 'unauthenticated' && router.pathname !== '/') {
+      router.replace('/');
+    }
+  }, [status]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <Head>
@@ -18,8 +34,13 @@ export default function Layout({ children }) {
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className={styles.container}>{children}</div>
-      <Navbar />
+
+      <div className={styles.container}>
+        {router.pathname !== '/'
+          ? user && cloneElement(children, { user })
+          : cloneElement(children, { user })}
+      </div>
+      {user && <Navbar />}
     </>
   );
 }
