a9438f1b443b9bfcda0c48c761365955bbf9442f
ngvuong <vminguyen@ucdavis.edu>
2022-07-22 00:11:53 -0700

    Update _app with UserProvider context provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { SessionProvider } from 'next-auth/react';
+import { UserProvider } from '../contexts/userContext';
 import Layout from '../components/layout/Layout';
 
 import '../styles/globals.scss';
@@ -6,9 +7,11 @@ import '../styles/globals.scss';
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <UserProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </UserProvider>
     </SessionProvider>
   );
 }
