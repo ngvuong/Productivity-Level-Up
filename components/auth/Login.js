9f38f35bf7faf623e22c23aa970af489fd9c8dc6
ngvuong <vminguyen@ucdavis.edu>
2022-07-25 18:49:04 -0700

    Update Login with signin loading state

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import { signIn } from 'next-auth/react';
+import Overlay from '../layout/Overlay';
+import Spinner from '../layout/Spinner';
 
 import {
   FaGithub,
@@ -10,8 +13,20 @@ import {
 import styles from '../../styles/Login.module.scss';
 
 export default function Login({ setIsLogin }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSignIn = (method) => {
+    setIsLoading(true);
+    signIn(method);
+  };
+
   return (
     <div className={styles.login}>
+      {isLoading && (
+        <Overlay>
+          <Spinner />
+        </Overlay>
+      )}
       <h2>Log In</h2>
       <span>
         <hr />
@@ -19,15 +34,18 @@ export default function Login({ setIsLogin }) {
         <hr />
       </span>
       <div className={styles.loginBtns}>
-        <button className={styles.github} onClick={() => signIn('github')}>
+        <button className={styles.github} onClick={() => onSignIn('github')}>
           <FaGithub />
           GitHub
         </button>
-        <button className={styles.google} onClick={() => signIn('google')}>
+        <button className={styles.google} onClick={() => onSignIn('google')}>
           <FaGoogle />
           Google
         </button>
-        <button className={styles.facebook} onClick={() => signIn('facebook')}>
+        <button
+          className={styles.facebook}
+          onClick={() => onSignIn('facebook')}
+        >
           <FaFacebook />
           Facebook
         </button>
