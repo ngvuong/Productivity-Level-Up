0332cd8c925fd069ad49db58392448f915b012e1
ngvuong <vminguyen@ucdavis.edu>
2022-07-27 21:26:44 -0700

    Update Register with signup/signin handler

--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+import { signIn } from 'next-auth/react';
+import Overlay from '../layout/Overlay';
+import Spinner from '../layout/Spinner';
+
 import { FaGithub, FaGoogle, FaFacebook, FaSignInAlt } from 'react-icons/fa';
 import styles from '../../styles/Register.module.scss';
 
 export default function Register({ setIsLogin }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSignIn = (method) => {
+    setIsLoading(true);
+    signIn(method);
+  };
+
   return (
     <div className={styles.register}>
+      {isLoading && (
+        <Overlay>
+          <Spinner />
+        </Overlay>
+      )}
       <h2>Sign Up</h2>
       <span>
         <hr />
@@ -11,15 +28,18 @@ export default function Register({ setIsLogin }) {
         <hr />
       </span>
       <div className={styles.loginBtns}>
-        <button className={styles.github}>
+        <button className={styles.github} onClick={() => onSignIn('github')}>
           <FaGithub />
           GitHub
         </button>
-        <button className={styles.google}>
+        <button className={styles.google} onClick={() => onSignIn('google')}>
           <FaGoogle />
           Google
         </button>
-        <button className={styles.facebook}>
+        <button
+          className={styles.facebook}
+          onClick={() => onSignIn('facebook')}
+        >
           <FaFacebook />
           Facebook
         </button>
