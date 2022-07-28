46264c85928f5fc220a11e4d720f5c5ed45b893a
ngvuong <vminguyen@ucdavis.edu>
2022-07-27 22:51:12 -0700

    Update home page with Overview component

--- pages/home.js
+++ pages/home.js
@@ -1,31 +1,12 @@
-import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import Level from '../components/ui/Level';
-import Task from '../components/main/Task';
+import Overview from '../components/main/Overview';
 
 import { FaUser } from 'react-icons/fa';
 import styles from '../styles/Home.module.scss';
 
 export default function Home({ user }) {
-  const [tasks, setTasks] = useState([
-    { name: 'Task 1', completed: false },
-    { name: 'Task 2', completed: true },
-    { name: 'Task 3', completed: false },
-  ]);
-  const completedTaskCount = tasks.filter((task) => task.completed).length;
-
-  const markTask = (task) => {
-    setTasks(
-      tasks.map((t) => {
-        if (t.name === task.name) {
-          return { ...t, completed: !t.completed };
-        }
-        return t;
-      })
-    );
-  };
-
   return (
     <main className={styles.home}>
       <header className={styles.header}>
@@ -34,7 +15,7 @@ export default function Home({ user }) {
             <h1>PLUM</h1>
             <h3>Productivity Level Up Mate</h3>
           </div>
-          <Link href='/tasks'>
+          <Link href='/todo'>
             <a className={styles.user}>
               {user.image ? (
                 <Image
@@ -53,26 +34,7 @@ export default function Home({ user }) {
         </div>
         <Level user={user} />
       </header>
-      <section className={styles.overview}>
-        <h2>Today</h2>
-        <div className={styles.stats}>
-          <div>8 Sessions Completed</div>
-          <div>{`${completedTaskCount} Task${
-            completedTaskCount !== 1 ? 's' : ''
-          } Completed`}</div>
-          <div>100 Exp Gained</div>
-          <div>360 Minutes Logged</div>
-        </div>
-        <h3>Tasks</h3>
-        <div className={styles.tasks}>
-          {tasks.map((task) => (
-            <Task key={task.name} task={task} markTask={markTask} />
-          ))}
-        </div>
-        <Link href='/timer'>
-          <a className={styles.track}>Start Session</a>
-        </Link>
-      </section>
+      <Overview />
     </main>
   );
 }
