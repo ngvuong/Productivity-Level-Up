295a76d0e6afcbcb3de6894c5d9b4d6f4ad726b2
ngvuong <vminguyen@ucdavis.edu>
2022-07-28 02:52:48 -0700

    Update Taskbar task priority, toggle done logic

--- components/ui/Taskbar.js
+++ components/ui/Taskbar.js
@@ -1,19 +1,38 @@
+import { useEffect, useState } from 'react';
 import { FaRegSquare, FaRegCheckSquare, FaCheckDouble } from 'react-icons/fa';
 import styles from '../../styles/Taskbar.module.scss';
 
-export default function Taskbar({ task, toggleDone }) {
-  const done = task.completed;
+export default function Taskbar({ task, toggleDone, toggleDetails }) {
+  const [done, setDone] = useState(task.completed);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => toggleDone(task.id, done), 600);
+
+    return () => clearTimeout(timeout);
+  }, [task.id, done, toggleDone]);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggleDetails}>
       <label>
         {done ? <FaRegCheckSquare /> : <FaRegSquare />}
         <input
           type='checkbox'
           checked={done}
-          onChange={() => toggleDone(task.id)}
+          onChange={(e) => setDone(e.target.checked)}
         />
       </label>
-      <div className={styles.taskBar}>
+      <div
+        className={styles.taskBar}
+        style={{
+          border: `3px solid ${
+            task.priority === 'LOW'
+              ? '#55ff00'
+              : task.priority === 'MEDIUM'
+              ? '#0055ff'
+              : '#ff0055'
+          }`,
+        }}
+      >
         <span className={done ? styles.done : ''}>{task.name}</span>
         <div className={`${styles.taskBarFill} ${done ? styles.full : ''}`} />
         {done && <FaCheckDouble />}
