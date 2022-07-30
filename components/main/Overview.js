bc70753278a2175d2dfd96bc422d364ae1df7cb0
ngvuong <vminguyen@ucdavis.edu>
2022-07-30 04:15:46 -0700

    Update Overview to use data from db, update logic

--- components/main/Overview.js
+++ components/main/Overview.js
@@ -1,26 +1,36 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Link from 'next/link';
+import { format } from 'date-fns';
 import Task from './Task';
+import Spinner from '../layout/Spinner';
+import useTasksByDate from '../../hooks/useTasksByDate';
 
 import styles from '../../styles/Overview.module.scss';
 
-export default function Overview() {
-  const [tasks, setTasks] = useState([
-    { id: 1, name: 'Task 1', completed: false },
-    { id: 2, name: 'Task 2', completed: true },
-    { id: 3, name: 'Task 3', completed: false },
-  ]);
+export default function Overview({ userId }) {
+  const { tasks, isLoading, setTasks } = useTasksByDate(
+    userId,
+    format(new Date(), 'yyyy-MM-dd'),
+    { revalidateOnMount: true }
+  );
+
+  if (isLoading) {
+    return <Spinner />;
+  }
   const completedTaskCount = tasks.filter((task) => task.completed).length;
 
-  const toggleDone = (id) => {
-    setTasks(
-      tasks.map((t) => {
-        if (t.id === id) {
-          return { ...t, completed: !t.completed };
-        }
-        return t;
-      })
-    );
+  const toggleDone = async (id, done) => {
+    const data = await fetch(`/api/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ completed: done }),
+    }).then((res) => res.json());
+
+    if (data.error) console.error(data.error);
+
+    setTasks();
   };
 
   return (
