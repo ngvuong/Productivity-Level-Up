840f9727f7b63d0e7b716025a2e1b864224d628e
ngvuong <vminguyen@ucdavis.edu>
2022-08-02 00:03:04 -0700

    Update Task edit functionality logic

--- components/main/Task.js
+++ components/main/Task.js
@@ -13,8 +13,15 @@ export default function Task({ task, userId, toggleDone }) {
   const [edit, setEdit] = useState(false);
   const [projectOptions, setProjectOptions] = useState([]);
   const [tagOptions, setTagOptions] = useState([]);
-  const { projects, setProjects } = useProjects(userId);
-  const { tags, setTags } = useTags(userId);
+  const { projects, setProjects } = useProjects(userId, {
+    revalidateOnMount: true,
+  });
+  const { tags, setTags } = useTags(userId, { revalidateOnMount: true });
+  const priorityOptions = [
+    { label: 'Low', value: 'low' },
+    { label: 'Medium', value: 'medium' },
+    { label: 'High', value: 'high' },
+  ];
   console.log(projects);
 
   useEffect(() => {
@@ -39,17 +46,23 @@ export default function Task({ task, userId, toggleDone }) {
   }, [tags]);
 
   const onNewProject = async (project) => {
+    const name = project
+      .trim()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+
     const data = await fetch(`/api/user/${userId}/projects`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ name: project }),
+      body: JSON.stringify({ name }),
     }).then((res) => res.json());
 
-    if (data.error) return console.error(data.error);
+    if (data.error) return alert(data.error);
 
-    const newOption = { label: project, value: data.id };
+    const newOption = { label: name, value: data.id };
     setProjectOptions([...projectOptions, newOption]);
 
     setProjects();
@@ -58,17 +71,23 @@ export default function Task({ task, userId, toggleDone }) {
   };
 
   const onNewTag = async (tag) => {
+    const name = tag
+      .trim()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+
     const data = await fetch(`/api/user/${userId}/tags`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ name: tag }),
+      body: JSON.stringify({ name }),
     }).then((res) => res.json());
 
-    if (data.error) return console.error(data.error);
+    if (data.error) return alert(data.error);
 
-    const newOption = { label: tag, value: data.id };
+    const newOption = { label: name, value: data.id };
     setTagOptions([...tagOptions, newOption]);
 
     setTags();
@@ -99,26 +118,30 @@ export default function Task({ task, userId, toggleDone }) {
                 </button>
                 <h3>{task.name}</h3>
                 <label>
-                  Project:{' '}
+                  Project
                   {edit ? (
                     <CustomSelect
                       name='project'
                       options={projectOptions}
                       onCreateNew={onNewProject}
-                      defaultValue={task.project}
+                      defaultValue={projectOptions.find(
+                        (p) => p.value === task.projectId
+                      )}
                     />
                   ) : (
                     task.project
                   )}
                 </label>
                 <label>
-                  Tags:
+                  Tags
                   {edit ? (
                     <CustomSelect
                       name='tag'
                       options={tagOptions}
                       onCreateNew={onNewTag}
-                      defaultValue={task.tags}
+                      defaultValue={tagOptions.filter((o) =>
+                        task.tags.some((t) => t.id === o.value)
+                      )}
                       multiple
                     />
                   ) : (
@@ -128,20 +151,33 @@ export default function Task({ task, userId, toggleDone }) {
                   )}
                 </label>
                 <label>
-                  Priority:{' '}
-                  <span>
-                    {task.priority === 'P1'
-                      ? 'High'
-                      : task.priority === 'P2'
-                      ? 'Medium'
-                      : 'Low'}
-                  </span>
+                  Priority
+                  {edit ? (
+                    <CustomSelect
+                      options={priorityOptions}
+                      defaultValue={
+                        task.priority === 'P3'
+                          ? priorityOptions[0]
+                          : task.priority === 'P2'
+                          ? priorityOptions[1]
+                          : priorityOptions[2]
+                      }
+                    />
+                  ) : (
+                    <span>
+                      {task.priority === 'P1'
+                        ? 'High'
+                        : task.priority === 'P2'
+                        ? 'Medium'
+                        : 'Low'}
+                    </span>
+                  )}
                 </label>
                 <label>
-                  Date: <span>{task.date}</span>
+                  Date {edit ? <input type='date' /> : <span>{task.date}</span>}
                 </label>
                 <label>
-                  Notes: <p>{task.notes}</p>
+                  Notes {edit ? <input type='text' /> : <p>{task.notes}</p>}
                 </label>
                 {edit && <button className={styles.btnSave}>Save</button>}
                 <button
