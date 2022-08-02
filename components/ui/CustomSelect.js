53ab6efa6faa455060460025cbef8c1cb39ff5d8
ngvuong <vminguyen@ucdavis.edu>
2022-08-02 01:46:07 -0700

    Update CustomSelect onChange, newOption logic

--- components/ui/CustomSelect.js
+++ components/ui/CustomSelect.js
@@ -1,14 +1,40 @@
 import { useState } from 'react';
 import CreatableSelect from 'react-select/creatable';
+import Select from 'react-select';
 
 const styles = {
   control: (styles) => ({
     ...styles,
     backgroundColor: '#1f1f1f',
+    borderRadius: '1rem',
   }),
-  menu: (styles) => ({ ...styles, backgroundColor: '#1f1f1f' }),
+  menuList: (styles) => ({
+    ...styles,
+    backgroundColor: '#1f1f1f',
+    maxHeight: '20rem',
+    overflow: 'auto',
+  }),
   input: (styles) => ({ ...styles, color: '#dcd7de' }),
-  singleValue: (styles) => ({ ...styles, color: '#c1a' }),
+  singleValue: (styles, { data }) => ({
+    ...styles,
+
+    color:
+      data.value === 'P3'
+        ? '#55ff00'
+        : data.value === 'P2'
+        ? '#0055ff'
+        : data.value === 'P1'
+        ? '#ff0055'
+        : '#c1a',
+  }),
+  multiValue: (styles) => ({
+    ...styles,
+    backgroundColor: '#1f1f1f',
+  }),
+  multiValueLabel: (styles) => ({
+    ...styles,
+    backgroundColor: '#fc0',
+  }),
   option: (styles, { isFocused, isSelected }) => ({
     ...styles,
     color: isSelected ? '#909' : 'inherit',
@@ -20,19 +46,60 @@ export default function CustomSelect({
   name,
   options,
   onCreateNew,
+  onInputChange,
   defaultValue,
+  multiple,
 }) {
-  const [selectedOption, setSelectedOption] = useState(defaultValue);
+  const [selected, setSelected] = useState(defaultValue);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onCreateOption = async (value) => {
+    setIsLoading(true);
+    const newOption = await onCreateNew(value);
+
+    if (multiple) {
+      setSelected([...selected, newOption]);
+    } else {
+      setSelected(newOption);
+    }
+
+    setIsLoading(false);
+  };
+
+  const onChange = (option) => {
+    setSelected(option);
+    if (multiple) {
+      onInputChange({ name, value: option.map((o) => o.value) });
+    } else {
+      onInputChange({ name, value: option.value });
+    }
+    console.log(option);
+  };
 
   return (
-    <CreatableSelect
-      value={selectedOption}
-      onChange={setSelectedOption}
-      options={options}
-      onCreateOption={onCreateNew}
-      isClearable
-      styles={styles}
-      placeholder={`Select or add a new ${name}`}
-    />
+    <>
+      {onCreateNew ? (
+        <CreatableSelect
+          value={selected}
+          onChange={onChange}
+          options={options}
+          onCreateOption={onCreateOption}
+          isLoading={isLoading}
+          isClearable
+          isMulti={multiple}
+          styles={styles}
+          placeholder={`Select or add ${
+            multiple ? 'new ' + name : 'a new ' + name
+          }`}
+        />
+      ) : (
+        <Select
+          value={selected}
+          onChange={onChange}
+          options={options}
+          styles={styles}
+        />
+      )}
+    </>
   );
 }
