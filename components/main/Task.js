b64b4ad4c4316eea3e780e199c5d395a80e7b684
ngvuong <vminguyen@ucdavis.edu>
2022-08-01 20:55:45 -0700

    Update Task with tags selection, creation logic

--- components/main/Task.js
+++ components/main/Task.js
@@ -1,11 +1,80 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Taskbar from '../ui/Taskbar';
 import Overlay from '../layout/Overlay';
+import CustomSelect from '../ui/CustomSelect';
+import useProjects from '../../hooks/useProjects';
+import useTags from '../../hooks/useTags';
 
+import { FaEdit } from 'react-icons/fa';
 import styles from '../../styles/Task.module.scss';
 
-export default function Task({ task, toggleDone }) {
+export default function Task({ task, userId, toggleDone }) {
   const [showDetails, setShowDetails] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [projectOptions, setProjectOptions] = useState([]);
+  const [tagOptions, setTagOptions] = useState([]);
+  const { projects, setProjects } = useProjects(userId);
+  const { tags, setTags } = useTags(userId);
+  console.log(projects);
+
+  useEffect(() => {
+    if (projects) {
+      const options = projects.map((project) => ({
+        label: project.name,
+        value: project.id,
+      }));
+      setProjectOptions(options);
+    }
+  }, [projects]);
+
+  useEffect(() => {
+    if (tags) {
+      const options = tags.map((tag) => ({
+        label: tag.name,
+        value: tag.id,
+      }));
+
+      setTagOptions(options);
+    }
+  }, [tags]);
+
+  const onNewProject = async (project) => {
+    const data = await fetch(`/api/user/${userId}/projects`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: project }),
+    }).then((res) => res.json());
+
+    if (data.error) return console.error(data.error);
+
+    const newOption = { label: project, value: data.id };
+    setProjectOptions([...projectOptions, newOption]);
+
+    setProjects();
+
+    return newOption;
+  };
+
+  const onNewTag = async (tag) => {
+    const data = await fetch(`/api/user/${userId}/tags`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: tag }),
+    }).then((res) => res.json());
+
+    if (data.error) return console.error(data.error);
+
+    const newOption = { label: tag, value: data.id };
+    setTagOptions([...tagOptions, newOption]);
+
+    setTags();
+
+    return newOption;
+  };
 
   return (
     <div className={styles.task}>
@@ -22,12 +91,41 @@ export default function Task({ task, toggleDone }) {
                 <h3>{task.name}</h3>
               </div>
               <div className={styles.cardBack}>
-                <label>Project: {task.project}</label>
+                <button
+                  className={styles.btnEdit}
+                  onClick={() => setEdit(!edit)}
+                >
+                  <FaEdit />
+                </button>
+                <h3>{task.name}</h3>
                 <label>
-                  Tags:{' '}
-                  <div>
-                    {task.tags && task.tags.map((tag) => tag.name).join(', ')}
-                  </div>
+                  Project:{' '}
+                  {edit ? (
+                    <CustomSelect
+                      name='project'
+                      options={projectOptions}
+                      onCreateNew={onNewProject}
+                      defaultValue={task.project}
+                    />
+                  ) : (
+                    task.project
+                  )}
+                </label>
+                <label>
+                  Tags:
+                  {edit ? (
+                    <CustomSelect
+                      name='tag'
+                      options={tagOptions}
+                      onCreateNew={onNewTag}
+                      defaultValue={task.tags}
+                      multiple
+                    />
+                  ) : (
+                    <div>
+                      {task.tags && task.tags.map((tag) => tag.name).join(', ')}
+                    </div>
+                  )}
                 </label>
                 <label>
                   Priority:{' '}
@@ -45,9 +143,13 @@ export default function Task({ task, toggleDone }) {
                 <label>
                   Notes: <p>{task.notes}</p>
                 </label>
+                {edit && <button className={styles.btnSave}>Save</button>}
                 <button
                   className={styles.btnClose}
-                  onClick={() => setShowDetails(false)}
+                  onClick={() => {
+                    setEdit(false);
+                    setShowDetails(false);
+                  }}
                 >
                   ✕
                 </button>
