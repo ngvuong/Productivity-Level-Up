f1d4a65578dc163373b569ef78d1e2b31e4d5342
ngvuong <vminguyen@ucdavis.edu>
2022-08-07 04:07:01 -0700

    Update todo page with tasks data fetching, filtering

--- pages/todo.js
+++ pages/todo.js
@@ -1,22 +1,56 @@
 import { useState } from 'react';
-import { format } from 'date-fns';
+import { format, parseISO } from 'date-fns';
+import Select from 'react-select';
+import Spinner from '../components/layout/Spinner';
 import Overlay from '../components/layout/Overlay';
 import TaskCard from '../components/ui/TaskCard';
+import useTasks from '../hooks/useTasks';
 
 import { FaPlus } from 'react-icons/fa';
 import styles from '../styles/Todo.module.scss';
+import Task from '../components/main/Task';
 
 export default function Todo({ user }) {
+  const today = format(new Date(), 'yyyy-MM-dd');
   const [date, setDate] = useState('');
   const [showCard, setShowCard] = useState(false);
+  const [selected, setSelected] = useState({ label: 'Today', value: today });
   const [taskDetails, setTaskDetails] = useState({
     name: '',
     project: null,
     tags: [],
     priority: 'P3',
-    date: format(new Date(), 'yyyy-MM-dd'),
+    date: today,
     notes: '',
   });
+  const { tasks, isLoading, setTasks } = useTasks(user.id, {
+    revalidateOnMount: true,
+  });
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  const tasksByDate = tasks.reduce((acc, task) => {
+    if (task.date >= today) {
+      if (!acc[task.date]) {
+        acc[task.date] = [];
+      }
+
+      acc[task.date].push(task);
+    }
+
+    return acc;
+  }, {});
+
+  const options = Object.keys(tasksByDate)
+    .sort()
+    .map((date) => ({
+      label: date === today ? 'Today' : date,
+      value: date,
+    }));
+
+  const onChange = () => {};
 
   const getData = () => {
     const { name, project, tags, priority, date, notes } = taskDetails;
@@ -40,7 +74,7 @@ export default function Todo({ user }) {
 
   const onSave = async () => {
     const data = getData();
-    console.log(data);
+
     if (!data.name || !data.priority || !data.date) return;
 
     const result = await fetch(`/api/user/${user.id}/tasks`, {
@@ -57,8 +91,8 @@ export default function Todo({ user }) {
   return (
     <main className={styles.todo}>
       <h1>Manage tasks</h1>
-      <section>
-        <div>
+      <section className={styles.tasks}>
+        <div className={styles.configs}>
           <button onClick={() => setShowCard(!showCard)}>
             <FaPlus />
           </button>
@@ -74,8 +108,12 @@ export default function Todo({ user }) {
               />
             </Overlay>
           )}
+          <Select value={selected} onChange={onChange} options={options} />
         </div>
         <h2>Today</h2>
+        {tasksByDate[selected.value].map((task) => (
+          <Task key={task.id} task={task} setTasks={setTasks} />
+        ))}
       </section>
     </main>
   );
