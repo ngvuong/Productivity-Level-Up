275b94720ed64356f2f9d99308121da9dd74d794
ngvuong <vminguyen@ucdavis.edu>
2022-08-10 00:43:10 -0700

    Update TaskCard new task select logic

--- components/ui/TaskCard.js
+++ components/ui/TaskCard.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, forwardRef } from 'react';
 import { format, parseISO } from 'date-fns';
 import CustomSelect from '../ui/CustomSelect';
 import useProjects from '../../hooks/useProjects';
@@ -7,269 +7,325 @@ import useTags from '../../hooks/useTags';
 import { FaEdit } from 'react-icons/fa';
 import styles from '../../styles/TaskCard.module.scss';
 
-export default function TaskCard({
-  task,
-  userId,
-  setShowCard,
-  taskDetails,
-  setTaskDetails,
-  onSave,
-  valid,
-}) {
-  const [edit, setEdit] = useState(task ? false : true);
-  const [projectOptions, setProjectOptions] = useState([]);
-  const [tagOptions, setTagOptions] = useState([]);
-  const [defaultDetails, setDefaultDetails] = useState(taskDetails);
-  const { projects, setProjects } = useProjects(userId, {
-    revalidateOnMount: true,
-  });
-  const { tags, setTags } = useTags(userId, { revalidateOnMount: true });
-  const priorityOptions = [
-    { label: 'Low', value: 'P3' },
-    { label: 'Medium', value: 'P2' },
-    { label: 'High', value: 'P1' },
-  ];
+const TaskCard = forwardRef(
+  (
+    {
+      task,
+      tasks,
+      userId,
+      setShowCard,
+      taskDetails,
+      setTaskDetails,
+      onSave,
+      valid,
+    },
+    ref
+  ) => {
+    const [edit, setEdit] = useState(task ? false : true);
+    const [projectOptions, setProjectOptions] = useState([]);
+    const [tagOptions, setTagOptions] = useState([]);
+    const [defaultDetails, setDefaultDetails] = useState(taskDetails);
 
-  useEffect(() => {
-    if (projects && !projectOptions.length) {
-      const options = projects.map((project) => ({
-        label: project.name,
-        value: project.id,
-      }));
-      setProjectOptions(options);
-    }
-    if (tags && !tagOptions.length) {
-      const options = tags.map((tag) => ({
-        label: tag.name,
-        value: tag.id,
-      }));
+    const { projects, setProjects } = useProjects(userId, {
+      revalidateOnMount: true,
+    });
 
-      setTagOptions(options);
-    }
-  }, [projects, tags]);
+    const { tags, setTags } = useTags(userId, { revalidateOnMount: true });
 
-  const onNewProject = async (project) => {
-    const name = project
-      .trim()
-      .split(' ')
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
+    const taskOptions =
+      tasks &&
+      [...tasks]
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((task) => ({
+          label: task.name,
+          value: task.id,
+        }));
 
-    const data = await fetch(`/api/user/${userId}/projects`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name }),
-    }).then((res) => res.json());
+    const priorityOptions = [
+      { label: 'Low', value: 'P3' },
+      { label: 'Medium', value: 'P2' },
+      { label: 'High', value: 'P1' },
+    ];
 
-    if (data.error) return alert(data.error);
+    useEffect(() => {
+      if (projects && !projectOptions.length) {
+        const options = projects.map((project) => ({
+          label: project.name,
+          value: project.id,
+        }));
 
-    const newOption = { label: name, value: data.id };
-    setProjectOptions([...projectOptions, newOption]);
+        setProjectOptions(options);
+      }
 
-    setProjects();
+      if (tags && !tagOptions.length) {
+        const options = tags.map((tag) => ({
+          label: tag.name,
+          value: tag.id,
+        }));
 
-    return newOption;
-  };
+        setTagOptions(options);
+      }
 
-  const onNewTag = async (tag) => {
-    const name = tag
-      .trim()
-      .split(' ')
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
+      return () => setTaskDetails(defaultDetails);
+    }, [projects, projectOptions, tags, tagOptions]);
 
-    const data = await fetch(`/api/user/${userId}/tags`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name }),
-    }).then((res) => res.json());
+    const onNewProject = async (project) => {
+      const name = project
+        .trim()
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
 
-    if (data.error) return alert(data.error);
+      const data = await fetch(`/api/user/${userId}/projects`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name }),
+      }).then((res) => res.json());
 
-    const newOption = { label: name, value: data.id };
-    setTagOptions([...tagOptions, newOption]);
+      if (data.error) return alert(data.error);
 
-    setTags();
+      const newOption = { label: name, value: data.id };
+      setProjectOptions([...projectOptions, newOption]);
 
-    return newOption;
-  };
+      setProjects();
 
-  const onDetailChange = (target) => {
-    setTaskDetails({ ...taskDetails, [target.name]: target.value });
-  };
+      return newOption;
+    };
 
-  return (
-    <div className={styles.taskCard}>
-      <div
-        className={`${styles.taskCardInner} ${task && edit ? styles.edit : ''}`}
-      >
-        {task && (
-          <div className={styles.cardFront}>
-            <h3>{task.name}</h3>
+    const onNewTag = async (tag) => {
+      const name = tag
+        .trim()
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+
+      const data = await fetch(`/api/user/${userId}/tags`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name }),
+      }).then((res) => res.json());
+
+      if (data.error) return alert(data.error);
+
+      const newOption = { label: name, value: data.id };
+      setTagOptions([...tagOptions, newOption]);
+
+      setTags();
+
+      return newOption;
+    };
+
+    const onDetailChange = (target) => {
+      if (target.name === 'task') {
+        const task = tasks.find((task) => task.id === target.value);
+
+        if (!task) return setTaskDetails(defaultDetails);
+
+        setTaskDetails({
+          ...task,
+          project: task?.project?.id,
+          tags: task.tags.map((tag) => tag.id),
+          date: format(new Date(), 'yyyy-MM-dd'),
+        });
+      } else {
+        setTaskDetails({ ...taskDetails, [target.name]: target.value });
+      }
+    };
+
+    return (
+      <div className={styles.taskCard} ref={ref}>
+        <div
+          className={`${styles.taskCardInner} ${
+            task && edit ? styles.edit : ''
+          }`}
+        >
+          {task && (
+            <div className={styles.cardFront}>
+              <h3>{task.name}</h3>
+              <label>
+                Project
+                {task.project && (
+                  <span className={styles.project}>{task?.project?.name}</span>
+                )}
+              </label>
+              <label>
+                Tags
+                <div className={styles.tags}>
+                  {task.tags &&
+                    task.tags.map((tag) => (
+                      <span key={tag.id}>{tag.name}</span>
+                    ))}
+                </div>
+              </label>
+              <label>
+                Priority
+                <span>
+                  {task.priority === 'P3'
+                    ? 'Low'
+                    : task.priority === 'P2'
+                    ? 'Medium'
+                    : 'High'}
+                  <style jsx>{`
+                    span {
+                      color: ${task.priority === 'P3'
+                        ? '#8f0'
+                        : task.priority === 'P2'
+                        ? '#08f'
+                        : '#f08'};
+                    }
+                  `}</style>
+                </span>
+              </label>
+              <label>
+                Date{' '}
+                <span className={styles.date}>
+                  {format(parseISO(task.date), 'MMM d, yyyy')}
+                </span>
+              </label>
+              <label>
+                Notes <p>{task.notes}</p>
+              </label>
+              {!edit && (
+                <button
+                  className={styles.btnEdit}
+                  onClick={() => setEdit(!edit)}
+                >
+                  <FaEdit />
+                </button>
+              )}
+              {!edit && (
+                <button
+                  className={styles.btnClose}
+                  onClick={() => setShowCard(false)}
+                >
+                  ✕
+                </button>
+              )}
+            </div>
+          )}
+
+          <div className={`${task ? styles.cardBack : styles.cardFront}`}>
+            {!task && (
+              <>
+                <h2>Add or Select Task</h2>
+                <CustomSelect
+                  name='task'
+                  options={taskOptions}
+                  onInputChange={onDetailChange}
+                  isClearable
+                />
+              </>
+            )}
+            <input
+              type='text'
+              name='name'
+              value={taskDetails.name}
+              onChange={(e) => onDetailChange(e.target)}
+              placeholder='Task name'
+            />
             <label>
               Project
-              {task.project && (
-                <span className={styles.project}>{task?.project?.name}</span>
-              )}
+              <CustomSelect
+                name='project'
+                options={projectOptions}
+                onCreateNew={onNewProject}
+                onInputChange={onDetailChange}
+                defaultValue={
+                  projectOptions.find((p) => p.value === taskDetails.project) ||
+                  null
+                }
+              />
             </label>
             <label>
               Tags
-              <div className={styles.tags}>
-                {task.tags &&
-                  task.tags.map((tag) => <span key={tag.id}>{tag.name}</span>)}
-              </div>
+              <CustomSelect
+                name='tags'
+                options={tagOptions}
+                onCreateNew={onNewTag}
+                onInputChange={onDetailChange}
+                defaultValue={tagOptions.filter((o) =>
+                  taskDetails.tags.some((t) => t === o.value)
+                )}
+                multiple
+              />
             </label>
             <label>
               Priority
-              <span>
-                {task.priority === 'P3'
-                  ? 'Low'
-                  : task.priority === 'P2'
-                  ? 'Medium'
-                  : 'High'}
-                <style jsx>{`
-                  span {
-                    color: ${task.priority === 'P3'
-                      ? '#8f0'
-                      : task.priority === 'P2'
-                      ? '#08f'
-                      : '#f08'};
-                  }
-                `}</style>
-              </span>
+              <CustomSelect
+                name='priority'
+                options={priorityOptions}
+                onInputChange={onDetailChange}
+                defaultValue={
+                  taskDetails.priority === 'P3'
+                    ? priorityOptions[0]
+                    : taskDetails.priority === 'P2'
+                    ? priorityOptions[1]
+                    : priorityOptions[2]
+                }
+              />
             </label>
             <label>
               Date{' '}
-              <span className={styles.date}>
-                {format(parseISO(task.date), 'MMM d, yyyy')}
-              </span>
+              <input
+                type='date'
+                name='date'
+                value={taskDetails.date}
+                onChange={(e) => onDetailChange(e.target)}
+              />
             </label>
             <label>
-              Notes <p>{task.notes}</p>
+              Notes{' '}
+              <input
+                type='text'
+                name='notes'
+                value={taskDetails.notes || ''}
+                onChange={(e) => onDetailChange(e.target)}
+                placeholder='Task notes'
+              />
             </label>
-            {!edit && (
-              <button className={styles.btnEdit} onClick={() => setEdit(!edit)}>
+            <button
+              className={styles.btnSave}
+              onClick={async () => {
+                await onSave();
+                setDefaultDetails(taskDetails);
+                task ? setEdit(false) : setShowCard(false);
+              }}
+              disabled={!valid}
+            >
+              Save
+            </button>
+            {task && (
+              <button
+                className={styles.btnEdit}
+                onClick={() => {
+                  setEdit(!edit);
+                  setTaskDetails(defaultDetails);
+                }}
+              >
                 <FaEdit />
               </button>
             )}
-            {!edit && (
+            {(edit || !task) && (
               <button
                 className={styles.btnClose}
-                onClick={() => setShowCard(false)}
+                onClick={() => {
+                  setShowCard(false);
+                }}
               >
                 ✕
               </button>
             )}
           </div>
-        )}
-
-        <div className={`${task ? styles.cardBack : styles.cardFront}`}>
-          <input
-            type='text'
-            name='name'
-            value={taskDetails.name}
-            onChange={(e) => onDetailChange(e.target)}
-            placeholder='Task name'
-          />
-          <label>
-            Project
-            <CustomSelect
-              name='project'
-              options={projectOptions}
-              onCreateNew={onNewProject}
-              onInputChange={onDetailChange}
-              defaultValue={
-                projectOptions.find((p) => p.value === taskDetails.project) ||
-                null
-              }
-            />
-          </label>
-          <label>
-            Tags
-            <CustomSelect
-              name='tags'
-              options={tagOptions}
-              onCreateNew={onNewTag}
-              onInputChange={onDetailChange}
-              defaultValue={tagOptions.filter((o) =>
-                taskDetails.tags.some((t) => t === o.value)
-              )}
-              multiple
-            />
-          </label>
-          <label>
-            Priority
-            <CustomSelect
-              name='priority'
-              options={priorityOptions}
-              onInputChange={onDetailChange}
-              defaultValue={
-                taskDetails.priority === 'P3'
-                  ? priorityOptions[0]
-                  : taskDetails.priority === 'P2'
-                  ? priorityOptions[1]
-                  : priorityOptions[2]
-              }
-            />
-          </label>
-          <label>
-            Date{' '}
-            <input
-              type='date'
-              name='date'
-              value={taskDetails.date}
-              onChange={(e) => onDetailChange(e.target)}
-            />
-          </label>
-          <label>
-            Notes{' '}
-            <input
-              type='text'
-              name='notes'
-              value={taskDetails.notes || ''}
-              onChange={(e) => onDetailChange(e.target)}
-              placeholder='Task notes'
-            />
-          </label>
-          <button
-            className={styles.btnSave}
-            onClick={async () => {
-              await onSave();
-              setDefaultDetails(taskDetails);
-              task ? setEdit(false) : setShowCard(false);
-            }}
-            disabled={!valid}
-          >
-            Save
-          </button>
-          {task && (
-            <button
-              className={styles.btnEdit}
-              onClick={() => {
-                setEdit(!edit);
-                setTaskDetails(defaultDetails);
-              }}
-            >
-              <FaEdit />
-            </button>
-          )}
-          {(edit || !task) && (
-            <button
-              className={styles.btnClose}
-              onClick={() => {
-                setShowCard(false);
-                setTaskDetails(defaultDetails);
-              }}
-            >
-              ✕
-            </button>
-          )}
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
+
+TaskCard.displayName = 'TaskCard';
+
+export default TaskCard;
