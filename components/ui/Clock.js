eb618e94b1450774505a796a93087ef00a39057a
ngvuong <vminguyen@ucdavis.edu>
2022-08-17 22:40:26 -0700

    Update Clock with ticking effects

--- components/ui/Clock.js
+++ components/ui/Clock.js
@@ -1,7 +1,41 @@
+import { useState, useEffect } from 'react';
+
 import { RiTimerFlashLine } from 'react-icons/ri';
 import styles from '../../styles/Clock.module.scss';
 
-export default function Clock({ time, totalTime }) {
+export default function Clock({ time, totalTime, run }) {
+  const [seconds, setSeconds] = useState(time);
+  const [secondsTick, setSecondsTick] = useState(false);
+  const [minutesTick, setMinutesTick] = useState(false);
+
+  useEffect(() => {
+    setSecondsTick(true);
+
+    const timeout = setTimeout(() => {
+      setSecondsTick(false);
+      setSeconds(time);
+    }, 210);
+
+    return () => clearTimeout(timeout);
+  }, [time]);
+
+  useEffect(() => {
+    if (time % 60 === 59) {
+      setMinutesTick(true);
+      // const timeout = setTimeout(() => {
+      //   setMinutesTick(true);
+      // }, 1000);
+
+      // return () => clearTimeout(timeout);
+    } else if (minutesTick) {
+      const timeout = setTimeout(() => {
+        setMinutesTick(false);
+      }, 1000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [time, minutesTick]);
+
   return (
     <div className={styles.clock}>
       {totalTime && (
@@ -22,19 +56,35 @@ export default function Clock({ time, totalTime }) {
           time === 0 ? styles.done : ''
         }`}
       >
-        <span className={styles.minutes}>
-          {`${Math.floor(time / 60).toLocaleString('en-US', {
-            minimumIntegerDigits: 2,
-            useGroup: false,
-          })}`}
-        </span>
+        <div className={styles.minutes}>
+          <span className={`${minutesTick ? styles.tick : ''}`}>
+            {`${Math.floor(time / 60).toLocaleString('en-US', {
+              minimumIntegerDigits: 2,
+              useGroup: false,
+            })}`}
+          </span>
+          <span className={`${minutesTick ? styles.tick : ''}`}>
+            {`${Math.floor(seconds / 60).toLocaleString('en-US', {
+              minimumIntegerDigits: 2,
+              useGroup: false,
+            })}`}
+          </span>
+        </div>
         {totalTime && ':'}
-        <span className={styles.seconds}>
-          {`${(time % 60).toLocaleString('en-US', {
-            minimumIntegerDigits: 2,
-            useGroup: false,
-          })}`}
-        </span>
+        <div className={styles.seconds}>
+          <span className={`${secondsTick ? styles.tick : ''}`}>
+            {`${(time % 60).toLocaleString('en-US', {
+              minimumIntegerDigits: 2,
+              useGroup: false,
+            })}`}
+          </span>
+          <span className={`${secondsTick ? styles.tick : ''}`}>
+            {`${(seconds % 60).toLocaleString('en-US', {
+              minimumIntegerDigits: 2,
+              useGroup: false,
+            })}`}
+          </span>
+        </div>
       </div>
     </div>
   );
