b697e220fdf8afda6439309ecd41c1c5bca61b9d
ngvuong <vminguyen@ucdavis.edu>
2022-08-21 04:05:19 -0700

    Update pomodoro page with stats

--- pages/pomodoro.js
+++ pages/pomodoro.js
@@ -1,15 +1,26 @@
+import { useEffect } from 'react';
 import Timer from '../components/ui/Timer';
+import Stat from '../components/ui/Stat';
+import { useTimer } from '../contexts/timerContext';
 
 import styles from '../styles/Pomodoro.module.scss';
 
 export default function Pomodoro({ user }) {
+  const [{ count, totalTime }] = useTimer();
+
   return (
     <main className={styles.pomodoro}>
       <section className={styles.timer}>
-        <Timer user={user} />
+        <Timer
+          defaultTime={user.settings.pomodoro}
+          defaultBreak={user.settings.breakTime}
+        />
       </section>
 
-      <section className={styles.stats}></section>
+      <section className={styles.stats}>
+        <Stat stat={count} label='session' />
+        <Stat stat={Math.floor(totalTime / 60)} label='minute' />
+      </section>
     </main>
   );
 }
