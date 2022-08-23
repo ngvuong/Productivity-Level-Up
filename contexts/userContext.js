387e34343247f3dea17651545ee0451da48442b2
ngvuong <vminguyen@ucdavis.edu>
2022-08-22 22:09:24 -0700

    Update userContext loading state, pass props

--- contexts/userContext.js
+++ contexts/userContext.js
@@ -1,16 +1,31 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, cloneElement } from 'react';
 import { useSession } from 'next-auth/react';
+import Overlay from '../components/layout/Overlay';
+import Spinner from '../components/layout/Spinner';
 
 const userContext = createContext();
 
 export function UserProvider({ children }) {
   const { data: session, status } = useSession();
 
+  if (status === 'loading') {
+    return (
+      <Overlay>
+        <Spinner />
+      </Overlay>
+    );
+  }
+
   const value = {
     user: status === 'authenticated' ? session.user : undefined,
     status,
   };
-  return <userContext.Provider value={value}>{children}</userContext.Provider>;
+
+  return (
+    <userContext.Provider value={value}>
+      {cloneElement(children, { user: value.user })}
+    </userContext.Provider>
+  );
 }
 
 export function useUser() {
