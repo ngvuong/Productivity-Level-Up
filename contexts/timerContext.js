f045ce97cb4395494f3366544349b0bcbb1ccc16
ngvuong <vminguyen@ucdavis.edu>
2022-08-24 01:44:42 -0700

    Update timerContext logic

--- contexts/timerContext.js
+++ contexts/timerContext.js
@@ -4,8 +4,6 @@ import {
   useRef,
   createContext,
   useContext,
-  cloneElement,
-  Children,
 } from 'react';
 
 const timerContext = createContext();
@@ -16,7 +14,6 @@ const timerReducer = (state, action) => {
       return {
         ...state,
         run: true,
-        skip: false,
       };
     case 'STOP_TIMER':
       return {
@@ -27,11 +24,7 @@ const timerReducer = (state, action) => {
       return {
         ...state,
         time: action.time,
-      };
-    case 'SKIP_TIME':
-      return {
-        ...state,
-        skip: true,
+        change: 'time',
       };
     case 'SET_TOTAL_TIME':
       return {
@@ -47,6 +40,7 @@ const timerReducer = (state, action) => {
       return {
         ...state,
         inSession: state.breakTime ? !state.inSession : true,
+        change: 'inSession',
       };
     case 'SET_POMODORO':
       return {
@@ -89,29 +83,16 @@ const timerReducer = (state, action) => {
 };
 
 export const TimerProvider = ({ user, children }) => {
-  const [state, dispatch] = useReducer(
-    timerReducer,
-    user
-      ? {
-          time: user.settings.time || user.settings.pomodoro,
-          run: false,
-          skip: false,
-          totalTime: 0,
-          count: 0,
-          inSession: true,
-          pomodoro: user.settings.pomodoro,
-          breakTime: user.settings.breakTime,
-          autostart: user.settings.autostart,
-          alarm: user.settings.alarm,
-          ticking: user.settings.ticking,
-        }
-      : {}
-  );
+  const { id, userId, ...settings } = user?.settings || {};
+  const [state, dispatch] = useReducer(timerReducer, {
+    ...settings,
+    count: 0,
+    totalTime: 0,
+  });
 
   const {
     time,
     run,
-    skip,
     totalTime,
     count,
     inSession,
@@ -125,7 +106,6 @@ export const TimerProvider = ({ user, children }) => {
 
   const alarmRef = useRef(null);
   const tickingRef = useRef(null);
-  const timeRef = useRef(time);
 
   useEffect(() => {
     if (alarm) {
@@ -138,53 +118,46 @@ export const TimerProvider = ({ user, children }) => {
   }, [alarm, ticking]);
 
   useEffect(() => {
-    if (run) {
+    if (run && time > 0) {
       const interval = setInterval(() => {
-        if (timeRef.current > 0) {
-          if (tickingRef.current) tickingRef.current.play();
+        if (tickingRef.current) tickingRef.current.play();
 
-          timeRef.current = timeRef.current - 1;
-          dispatch({ type: 'SET_TIME', time: timeRef.current });
-        } else clearInterval(interval);
+        dispatch({ type: 'SET_TIME', time: time - 1 });
       }, 1000);
-
-      if (timeRef.current === 0) return () => clearInterval(interval);
 
       return () => clearInterval(interval);
     }
-  }, [run]);
+  }, [time, run]);
 
   useEffect(() => {
-    if (time === 0 || skip) {
-      if (alarmRef.current && !skip) alarmRef.current.play();
-
-      dispatch({ type: 'STOP_TIMER' });
+    if (time === 0) {
+      if (alarmRef.current) alarmRef.current.play();
 
       const timeout = setTimeout(() => {
-        const timeNext = breakTime && inSession ? breakTime : pomodoro;
+        const shouldSwitch = breakTime && inSession;
+        const timeNext = shouldSwitch ? breakTime : pomodoro;
 
-        if (inSession && !skip) {
+        if (inSession) {
           dispatch({ type: 'SET_TOTAL_TIME', totalTime: totalTime + pomodoro });
           dispatch({ type: 'SET_COUNT', count: count + 1 });
         }
-        timeRef.current = timeNext;
 
         dispatch({ type: 'SET_TIME', time: timeNext });
 
-        dispatch({ type: 'SET_IN_SESSION' });
+        if (shouldSwitch || !inSession) dispatch({ type: 'SET_IN_SESSION' });
 
-        if (autostart) dispatch({ type: 'START_TIMER' });
+        if (!autostart) dispatch({ type: 'STOP_TIMER' });
       }, 1000);
 
       return () => clearTimeout(timeout);
     }
-  }, [time, skip, totalTime, count, inSession, pomodoro, breakTime, autostart]);
+  }, [time, totalTime, count, inSession, pomodoro, breakTime, autostart]);
 
   useEffect(() => {
     if (change) {
-      (async function update() {
+      const update = async () => {
         const result = await fetch(`api/user/${user.id}/settings`, {
-          method: 'POST',
+          method: 'PUT',
           headers: {
             'Content-Type': 'application/json',
           },
@@ -195,60 +168,15 @@ export const TimerProvider = ({ user, children }) => {
 
         if (result.error) console.error(result.error);
 
-        if (result.message) dispatch({ type: 'CLEAR_CHANGE' });
-      })();
+        if (result.ok) dispatch({ type: 'CLEAR_CHANGE' });
+      };
+
+      if (change !== 'time') update();
+
+      window.addEventListener('beforeunload', update);
+      return () => window.removeEventListener('beforeunload', update);
     }
-  }, [state, change]);
-  useEffect(() => {
-    const saveTime = async () => {
-      if (timeRef.current) {
-        const data = await fetch(`api/user/${user.id}/settings`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            time: timeRef.current,
-          }),
-        }).then((res) => res.json());
-
-        if (data.error) console.error(data.error);
-      }
-    };
-    console.log('hi');
-    window.addEventListener('beforeunload', saveTime);
-    return () => {
-      // if (unmountRef.current) console.log(unmountRef.current);
-      // saveTime();
-      console.log('bye');
-      window.removeEventListener('beforeunload', saveTime);
-    };
-  }, []);
-  // useEffect(() => {
-  //   localStorage.setItem('time', time);
-  // }, [time]);
-
-  // useEffect(() => {
-  //   // const time = timeRef.current;
-  //   // if (time % 1 === 0) {
-  //   // return () => {
-  //   console.log(localStorage.getItem('time'));
-  //   fetch(`api/user/${user.id}/settings`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       time,
-  //     }),
-  //   }).then((res) => {
-  //     const data = res.json();
-  //     // if (data.error) console.error(result.error);
-  //     data.then((msg) => console.log(msg));
-  //   });
-  //   // };
-  //   // }
-  // }, []);
+  }, [state, change, user]);
 
   const value = [state, dispatch];
 
