cef673355569827b42e5196c8f803824f06a2d15
ngvuong <vminguyen@ucdavis.edu>
2022-08-24 20:41:08 -0700

    Update user settings api handler req method

--- pages/api/user/[userid]/settings/index.js
+++ pages/api/user/[userid]/settings/index.js
@@ -1,15 +1,13 @@
 import prisma from '../../../../../lib/prisma';
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
+  if (req.method !== 'PUT') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
   try {
     const { userid } = req.query;
     const update = req.body;
-
-    console.log(update);
 
     await prisma.settings.update({
       where: {
@@ -18,7 +16,7 @@ export default async function handler(req, res) {
       data: update,
     });
 
-    return res.status(200).json({ message: 'Settings updated' });
+    return res.status(200).json({ ok: true });
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
