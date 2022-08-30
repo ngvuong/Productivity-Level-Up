90cd48d2c0e2c925ff46d92285466b259eccd38f
ngvuong <vminguyen@ucdavis.edu>
2022-08-29 20:07:55 -0700

    Add user info update api handler

---
+++ pages/api/user/[userid]/index.js
@@ -0,0 +1,20 @@
+import prisma from '../../../../lib/prisma';
+
+export default async function handler(req, res) {
+  if (req.method !== 'PUT') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+  try {
+    const { userid } = req.query;
+    const data = req.body;
+
+    await prisma.user.update({
+      where: { id: userid },
+      data,
+    });
+
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+}
