1f9f6617efcbaa603031344b2b78cdff17a3e502
ngvuong <vminguyen@ucdavis.edu>
2022-09-03 04:07:38 -0700

    Update Stat with transition useEffect logic

--- components/ui/Stat.js
+++ components/ui/Stat.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react';
+
 import styles from '../../styles/Stat.module.scss';
 
 export default function Stat({ stat, label }) {
+  const [current, setCurrent] = useState(stat);
+  const [change, setChange] = useState(false);
+
+  useEffect(() => {
+    if (stat !== current) {
+      setChange(true);
+
+      const timeout = setTimeout(() => {
+        setChange(false);
+        setCurrent(stat);
+      }, 200);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [stat, current]);
+
   return (
     <div className={styles.stat}>
-      {stat} {label + (stat !== 1 ? 's' : '')}
+      <span className={change ? styles.change : undefined}>
+        {current} {label + (current !== 1 ? 's' : '')}
+      </span>
+      <span className={change ? styles.change : undefined}>
+        {stat} {label + (stat !== 1 ? 's' : '')}
+      </span>
     </div>
   );
 }
