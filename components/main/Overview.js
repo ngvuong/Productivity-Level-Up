80479c457f9131b39300c40bffe949499c41c9fd
ngvuong <vminguyen@ucdavis.edu>
2022-09-07 22:11:33 -0700

    Update Overview tasks, stats from useSettings

--- components/main/Overview.js
+++ components/main/Overview.js
@@ -1,47 +1,57 @@
 import Link from 'next/link';
 import { format } from 'date-fns';
+import Overlay from '../layout/Overlay';
+import Spinner from '../layout/Spinner';
 import Stat from '../ui/Stat';
 import Task from './Task';
-import Spinner from '../layout/Spinner';
-import useTasksByDate from '../../hooks/useTasksByDate';
-import { useTimer } from '../../contexts/timerContext';
+import useTasks from '../../hooks/useTasks';
+import { useSettings } from '../../contexts/settingsContext';
 
 import styles from '../../styles/Overview.module.scss';
 
-export default function Overview({ userId }) {
-  const { tasks, isLoading } = useTasksByDate(
-    userId,
-    format(new Date(), 'yyyy-MM-dd'),
-    { revalidateOnMount: true }
-  );
+export default function Overview({ user }) {
+  const { tasks, isLoading, setTasks } = useTasks(user.id, {
+    revalidateOnMount: true,
+  });
 
-  const [{ totalTime, count }] = useTimer();
+  const [{ totalTime, count }] = useSettings();
 
   if (isLoading) {
-    return <Spinner />;
+    return (
+      <Overlay>
+        <Spinner />
+      </Overlay>
+    );
   }
 
-  const completedTaskCount = tasks.filter((task) => task.completed).length;
+  const today = format(new Date(), 'yyyy-MM-dd');
+
+  const tasksToday = tasks.filter((task) => task.date === today);
+
+  const completedTaskCount = tasksToday.filter((task) => task.completed).length;
 
   return (
     <section className={styles.overview}>
       <h2>Today</h2>
       <div className={styles.stats}>
-        <Stat stat={count} label='session' />
         <Stat stat={completedTaskCount} label='task' />
-        <Stat stat={100} label='exp' />
+        <Stat stat={count} label='session' />
         <Stat stat={Math.floor(totalTime / 60)} label='minute' />
+        <Stat stat={100} label='exp' />
+        <Stat stat={user.streak} label='non-zero day' />
       </div>
+
       <h3>Tasks</h3>
       <div className={styles.tasks}>
-        {tasks.length ? (
-          tasks.map((task) => (
-            <Task key={task.id} task={task} userId={userId} />
+        {tasksToday.length ? (
+          tasksToday.map((task) => (
+            <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks} />
           ))
         ) : (
           <p>No tasks yet</p>
         )}
       </div>
+
       <div className={styles.links}>
         <Link href='/todo'>
           <a className={styles.todo}>Manage Tasks</a>
