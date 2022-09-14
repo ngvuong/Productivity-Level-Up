d827c94abed7834922e3f4ab2ac0ac88a66a1358
ngvuong <vminguyen@ucdavis.edu>
2022-09-13 20:41:57 -0700

    Update Session animation, transition logic

--- components/main/Session.js
+++ components/main/Session.js
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useUser } from '../../contexts/userContext';
 
 import { MdWbSunny } from 'react-icons/md';
 import styles from '../../styles/Session.module.scss';
 
-export default function Session({ session }) {
+export default function Session({ session, timeClaimed, setTimeClaimed }) {
   const [claimed, setClaimed] = useState(false);
-  const [random, setRandom] = useState({
-    top: Math.floor(Math.random() * 101),
-    left: Math.floor(Math.random() * 101),
-  });
+  const [random, setRandom] = useState(Math.random());
+  //   top: Math.floor(Math.random() * 101),
+  //   left: Math.floor(Math.random() * 101),
+  // });
 
   const [user, dispatch] = useUser();
 
@@ -17,47 +17,66 @@ export default function Session({ session }) {
   const size = 1 + minutes / 20;
 
   useEffect(() => {
-    setRandom({
-      top: Math.floor(Math.random() * 101),
-      left: Math.floor(Math.random() * 101),
-    });
+    const interval = setInterval(
+      (function randomize() {
+        setRandom(Math.random());
 
-    const interval = setInterval(() => {
-      setRandom({
-        top: Math.floor(Math.random() * 101),
-        left: Math.floor(Math.random() * 101),
-      });
-    }, 20000);
+        return randomize;
+      })(),
+      30000
+    );
 
     return () => clearInterval(interval);
   }, []);
+  // const calculateExp = () => {
+  //   for (let i = 1; i <= minutes; i++) {}
+  // };
 
   const onClaim = async () => {
+    // setTimeClaimed(timeClaimed + minutes)
+    setClaimed(true);
+    // dispatch({ type: 'SET_EXP', exp: user.exp + 50 });
     // const result = await fetch(`api/pomodoros/${session.id}`, {
     //   method: 'PUT',
     //   headers: { 'Content-Type': 'application/json' },
     // }).then((res) => res.json());
     // if (result.error) console.error(result.error);
-
-    dispatch({ type: 'SET_EXP', exp: user.exp + 50 });
-    setClaimed(true);
   };
 
+  const getRandInt = (min = 0, max = 1) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+
+  const pulseAnimation = `
+    @keyframes pulse {
+      50% {
+        transform: scale(${+(random * 0.5).toFixed(2) + 1}) 
+        rotate(${getRandInt(360) * (getRandInt() ? 1 : -1)}deg);
+      }
+      100% {
+        transform: scale(1) 
+        rotate(0);
+      }
+    }
+  `;
+
   return (
-    <div
-      className={styles.session}
-      onClick={onClaim}
-      style={{
-        top: claimed ? '110%' : `min(${random.top}%, 100% - ${size / 2}rem)`,
-        left: claimed
-          ? `calc(50% - ${size / 2}rem)`
-          : `min(${random.left}%, 100% - ${size / 2}rem)`,
-        fontSize: `${size}rem`,
-        transitionDelay: `${Math.floor(Math.random() * 1001)}ms`,
-        animationDelay: `${Math.floor(Math.random() * 2001)}ms`,
-      }}
-    >
-      <MdWbSunny />
-    </div>
+    <>
+      <style children={pulseAnimation} />
+      <div
+        className={styles.session}
+        onClick={onClaim}
+        style={{
+          top: claimed ? '85%' : `min(${getRandInt(100)}% , 80% )`,
+          left: claimed
+            ? `calc(50% - ${size / 2}rem)`
+            : `min(${getRandInt(100)}%, 100% - ${size}rem)`,
+          fontSize: `${size}rem`,
+          animation: `pulse 10s  alternate-reverse infinite`,
+          transition: `all ${claimed ? 2 : 30}s cubic-bezier(0.65, 0, 0.35, 1)`,
+        }}
+      >
+        <MdWbSunny />
+      </div>
+    </>
   );
 }
