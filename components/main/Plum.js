16c19e5ea2eece295f671ec3e6838165d43c4420
ngvuong <vminguyen@ucdavis.edu>
2022-09-16 01:32:01 -0700

    Update Plum stats, plant svg styles, gradient

--- components/main/Plum.js
+++ components/main/Plum.js
@@ -1,45 +1,46 @@
 import { useState, useEffect } from 'react';
-import Session from '../ui/Session';
+import Stat from '../ui/Stat';
+import Session from './Session';
 import usePomodoros from '../../hooks/usePomodoros';
 
-import { RiSeedlingFill } from 'react-icons/ri';
+import { GiFruitTree } from 'react-icons/gi';
 import styles from '../../styles/Plum.module.scss';
 
 export default function Plum({ userId, date }) {
-  const [timeClaimed, setTimeClaimed] = useState();
-  const { pomodoros } = usePomodoros(userId, date, {
+  const [plum, setPlum] = useState();
+
+  const { pomodoros, setPomodoros } = usePomodoros(userId, date, {
     revalidateOnMount: true,
   });
 
-  const timeAvailable =
-    pomodoros &&
-    pomodoros.reduce(
-      (sum, curr) => (!curr.claimed ? sum + curr.duration : sum),
-      0
-    );
-
-  // let timeClaimed =
-  //   pomodoros &&
-  //   pomodoros.reduce(
-  //     (sum, curr) => (curr.claimed ? sum + curr.duration : sum),
-  //     0
-  //   );
-
   useEffect(() => {
     if (pomodoros) {
-      setTimeClaimed(
-        pomodoros.reduce(
-          (sum, curr) => (curr.claimed ? sum + curr.duration : sum),
-          0
-        )
-      );
+      const init = { timeAvailable: 0, timeClaimed: 0, expGained: 0 };
+
+      const plum = pomodoros.reduce((acc, curr) => {
+        if (curr.claimed) {
+          acc.timeClaimed += curr.duration / 60;
+
+          acc.expGained += curr.expGained;
+        } else {
+          acc.timeAvailable += curr.duration / 60;
+        }
+
+        return acc;
+      }, init);
+
+      setPlum(plum);
     }
-  }, []);
+  }, [pomodoros]);
 
   return (
     <div className={styles.plum}>
-      {timeAvailable !== undefined && (
-        <h2>{timeAvailable / 60} Minutes Available</h2>
+      {plum && (
+        <div className={styles.stats}>
+          <Stat stat={plum.timeAvailable} label='available minute' />
+          <Stat stat={plum.timeClaimed} label='claimed minute' />
+          <Stat stat={plum.expGained} label='gained exp' pluralize={false} />
+        </div>
       )}
 
       {pomodoros && (
@@ -47,21 +48,31 @@ export default function Plum({ userId, date }) {
           {pomodoros
             .filter((pomo) => !pomo.claimed)
             .map((pomo) => (
-              <Session key={pomo.id} session={pomo} />
+              <Session
+                key={pomo.id}
+                session={pomo}
+                // timeClaimed={timeClaimed}
+                // setTimeClaimed={setTimeClaimed}
+                setPomodoros={setPomodoros}
+              />
             ))}
         </div>
       )}
 
       <div className={styles.plant}>
-        <style jsx>{`
-          div {
-            transform: scale(${1 + Math.floor(timeClaimed / 1800) * 0.5});
-          }
-        `}</style>
-
-        <RiSeedlingFill />
+        <svg width='0' height='0'>
+          <linearGradient id='gradient' x1='0%' y1='100%' x2='0%' y2='50%'>
+            <stop stopColor='#742' offset='0%' />
+            <stop stopColor='#9ea' offset='100%' />
+          </linearGradient>
+        </svg>
+        <GiFruitTree
+          style={{
+            fill: 'url(#gradient)',
+            transform: `scale(${1 + Math.floor(plum?.timeClaimed / 5) * 0.01})`,
+          }}
+        />
       </div>
-      <button onClick={() => setTimeClaimed(timeClaimed + 1800)}>click</button>
     </div>
   );
 }
