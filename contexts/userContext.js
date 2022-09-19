49dd0b3ed7187437d5a29893fa8ca6c1eaf2c94c
ngvuong <vminguyen@ucdavis.edu>
2022-09-18 21:08:20 -0700

    Update userContext reducer, useEffect update logic

--- contexts/userContext.js
+++ contexts/userContext.js
@@ -6,44 +6,65 @@ import Spinner from '../components/layout/Spinner';
 const userContext = createContext();
 
 const userReducer = (state, action) => {
-  switch (action.type) {
-    case 'SET_LEVEL':
-      return {
-        ...state,
-        level: action.level,
-        change: 'level',
-      };
-    case 'SET_EXP':
-      const { level, expMin, expReq, expRate } = state;
-      const change = action.exp >= expMin + expReq ? 'levelUp' : 'exp';
+  const { type, user, exp, vExp, streak, streakDate } = action;
+
+  switch (type) {
+    case 'SET_EXP': {
+      let { level, expRate, expMin, expReq } = state;
+      let expNext = expMin + expReq;
+      let levelUp = exp >= expNext;
+
+      while (exp >= expNext) {
+        level++;
+        expRate = +(expRate + 0.2).toFixed(2);
+        expMin = expNext;
+        expReq = Math.ceil(+(expReq * 1.1).toFixed(2));
+        expNext += expReq;
+      }
 
       return {
         ...state,
-        exp: action.exp,
-        ...(change === 'levelUp' && {
-          level: level + 1,
-          expMin: expMin + expReq,
-          expReq: Math.ceil(expReq * 1.1),
-          expRate: expRate + 0.2,
-        }),
-        change,
+        exp,
+        ...(levelUp && { level, expRate, expMin, expReq }),
+        change: true,
       };
+    }
+    case 'SET_VIRTUAL_EXP': {
+      let { vLevel, vExpRate, vExpMin, vExpReq } = state;
+      let expNext = vExpMin + vExpReq;
+      let levelUp = vExp >= expNext;
+
+      while (vExp >= expNext) {
+        vLevel++;
+        vExpRate = +(vExpRate + 0.2).toFixed(2);
+        vExpMin = expNext;
+        vExpReq = Math.ceil(+(vExpReq * 1.1).toFixed(2));
+        expNext += vExpReq;
+      }
+
+      return {
+        ...state,
+        vExp,
+        ...(levelUp && { vLevel, vExpRate, vExpMin, vExpReq }),
+        change: true,
+      };
+    }
     case 'SET_STREAK':
       return {
         ...state,
-        streak: action.streak || 0,
-        streakDate: action.streakDate || null,
-        change: 'streak',
+        streak: streak || 0,
+        streakDate: streakDate || null,
+        change: true,
       };
     case 'SET_USER':
-      return action.user;
+      return user;
     case 'CLEAR_CHANGE':
       return {
         ...state,
-        change: '',
+        change: false,
       };
     default:
-      throw new Error(`Unhandled action type: ${action.type}`);
+      throw new Error(`Unhandled action type: ${type}`);
   }
 };
 
@@ -63,36 +84,34 @@ export function UserProvider({ children }) {
   useEffect(() => {
     if (state && state.change) {
       (async function update() {
-        const {
-          level,
-          exp,
-          expMin,
-          expReq,
-          expRate,
-          streak,
-          streakDate,
-          change,
-        } = state;
+        const data = [
+          'level',
+          'vLevel',
+          'exp',
+          'vExp',
+          'expRate',
+          'vExpRate',
+          'expMin',
+          'vExpMin',
+          'expReq',
+          'vExpReq',
+          'streak',
+          'streakDate',
+        ].reduce((a, c) => ((a[c] = state[c]), a), {});
+
+        const body = JSON.stringify(data);
 
         const result = await fetch(`api/user/${state.id}`, {
           method: 'PUT',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({
-            ...(change !== 'levelUp'
-              ? { [change]: state[change] }
-              : { level, exp, expMin, expReq, expRate }),
-            ...(change === 'streak' && { streakDate }),
-          }),
+          body,
         }).then((res) => res.json());
 
         if (result.error) console.error(result.error);
 
         if (result.success) dispatch({ type: 'CLEAR_CHANGE' });
-
-        if (change === 'streak')
-          dispatch({ type: 'SET_EXP', exp: exp + (level + expRate * streak) });
       })();
     }
   }, [state]);
