e89ca9fa557f3fc7f183839b34dd6079b2126b09
ngvuong <vminguyen@ucdavis.edu>
2022-09-25 22:40:37 -0700

    Update Stat singular label props

--- components/ui/Stat.js
+++ components/ui/Stat.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 import styles from '../../styles/Stat.module.scss';
 
-export default function Stat({ stat, label, pluralize = true }) {
+export default function Stat({ stat, label, singular = false }) {
   const [current, setCurrent] = useState(stat);
   const [change, setChange] = useState(false);
 
@@ -23,12 +23,12 @@ export default function Stat({ stat, label, pluralize = true }) {
     <div className={`${styles.stat} ${label ? styles.full : ''}`}>
       <span className={change ? styles.change : undefined}>
         {current +
-          (label ? ' ' + label + (current !== 1 && pluralize ? 's' : '') : '')}
+          (label ? ' ' + label + (current !== 1 && !singular ? 's' : '') : '')}
       </span>
 
       <span className={change ? styles.change : undefined}>
         {stat +
-          (label ? ' ' + label + (stat !== 1 && pluralize ? 's' : '') : '')}
+          (label ? ' ' + label + (stat !== 1 && !singular ? 's' : '') : '')}
       </span>
     </div>
   );
